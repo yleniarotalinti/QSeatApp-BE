3bdacb448d758bc3043cc9cfcd53dfbfe4742ac0
missyle <ylenia.rotalinti@gmail.com>
2021-01-24 14:43:21 +0100

    test

--- qseat_BE/src/main/java/com/qseat/QseatApplication.java
+++ qseat_BE/src/main/java/com/qseat/QseatApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class QseatApplication {
 
+	//commit test
 	public static void main(String[] args) {
 		SpringApplication.run(QseatApplication.class, args);
 	}
